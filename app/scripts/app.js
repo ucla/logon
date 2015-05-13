b5726a632548fc1f0a2c6a87e41e1ef7c8f16f70
Carl Uebelhart <carl@erutan.net>
2015-05-13 09:46:06 -0700

    stuff from before

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,28 @@
+/////////////////////////////////////////
+///     magnific popup
+/////////////////////////////////////////
+
+// set classes to be used to trigger overlays
 $('.ajax-popup').magnificPopup({
   type: 'ajax'
 });
+
+/////////////////////////////////////////
+///     load & configure webshims
+/////////////////////////////////////////
+
+// forms config
+webshim.setOptions('forms', {
+  //set lazyCustomMessages to true
+  lazyCustomMessages: true,
+  //show custom styleable validation bubble
+  replaceValidationUI: true,
+  handleBubble: 'hide',
+  fieldWrapper: '.fieldset',
+  iVal: {
+    'recheckDelay': 200,
+  }
+});
+
+// start polyfilling
+webshim.polyfill('forms forms-ext');
