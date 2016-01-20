b42809323ecf7f2851522aab5253550620be865a
Carl Uebelhart <carl@erutan.net>
2016-01-20 14:50:27 -0800

    run a AA accessibility check via 'npm start a11y'

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-var $        = require('gulp-load-plugins')();
-var argv     = require('yargs').argv;
-var browser  = require('browser-sync');
-var gulp     = require('gulp');
-var panini   = require('panini');
-var rimraf   = require('rimraf');
-var sequence = require('run-sequence');
-var sherpa   = require('style-sherpa');
+var $          = require('gulp-load-plugins')();
+var access     = require('gulp-accessibility');
+// var arialinter = require('gulp-arialinter');
+var argv       = require('yargs').argv;
+var browser    = require('browser-sync');
+var gulp       = require('gulp');
+var panini     = require('panini');
+var rimraf     = require('rimraf');
+var sequence   = require('run-sequence');
+var sherpa     = require('style-sherpa');
 
 // Check for --production flag
 var isProduction = !!(argv.production);
@@ -98,19 +100,24 @@ gulp.task('pages', function() {
       data: 'src/data/',
       helpers: 'src/helpers/'
     }))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .on('finish', browser.reload);
 });
 
-gulp.task('pages:reset', function(cb) {
+gulp.task('pages:reset', function(done) {
   panini.refresh();
-  gulp.run('pages', cb);
+  gulp.run('pages');
+  done();
 });
 
-gulp.task('styleguide', function(cb) {
+gulp.task('styleguide', function(done) {
   sherpa('src/styleguide/index.md', {
     output: 'dist/styleguide.html',
     template: 'src/styleguide/template.html'
-  }, cb);
+  }, function() {
+    browser.reload;
+    done();
+  });
 });
 
 // Compile Sass into CSS
@@ -119,7 +126,7 @@ gulp.task('sass', function() {
   var uncss = $.if(isProduction, $.uncss({
     html: ['src/**/*.html'],
     ignore: [
-      new RegExp('^meta\..*'),
+      new RegExp('.foundation-mq'),
       new RegExp('^\.is-.*')
     ]
   }));
@@ -139,7 +146,7 @@ gulp.task('sass', function() {
     .pipe(minifycss)
     .pipe($.if(!isProduction, $.sourcemaps.write()))
     .pipe(gulp.dest('dist/assets/css'))
-    .pipe(browser.reload({stream: true}));
+    .pipe(browser.reload({ stream: true }));
 });
 
 // Combine JavaScript into one file
@@ -155,7 +162,8 @@ gulp.task('javascript', function() {
     .pipe($.concat('app.js'))
     .pipe(uglify)
     .pipe($.if(!isProduction, $.sourcemaps.write()))
-    .pipe(gulp.dest('dist/assets/js'));
+    .pipe(gulp.dest('dist/assets/js'))
+    .on('finish', browser.reload);
 });
 
 // Copy images to the "dist" folder
@@ -167,7 +175,8 @@ gulp.task('images', function() {
 
   return gulp.src('src/assets/img/**/*')
     .pipe(imagemin)
-    .pipe(gulp.dest('dist/assets/img'));
+    .pipe(gulp.dest('dist/assets/img'))
+    .on('finish', browser.reload);
 });
 
 // Build the "dist" folder by running all of the above tasks
@@ -184,12 +193,21 @@ gulp.task('server', ['build'], function() {
 
 // Build the site, run the server, and watch for file changes
 gulp.task('default', ['build', 'server'], function() {
-  gulp.watch(PATHS.assets, ['copy', 'reload']);
+  gulp.watch(PATHS.assets, ['copy']);
   gulp.watch(PATHS.bowerDirectLinked, ['copy-bower', 'reload']);
-  gulp.watch(['src/pages/**/*.html'], ['pages', 'reload']);
-  gulp.watch(['src/{layouts,partials}/**/*.html'], ['pages:reset', 'reload']);
-  gulp.watch(['src/assets/scss/**/*.scss'], ['sass']);
-  gulp.watch(['src/assets/js/**/*.js'], ['javascript', 'reload']);
-  gulp.watch(['src/assets/img/**/*'], ['images', 'reload']);
-  gulp.watch(['src/styleguide/**'], ['styleguide', 'reload']);
+  gulp.watch(['src/pages/**/*'], ['pages']);
+  gulp.watch(['src/{layouts,partials,helpers,data}/**/*'], ['pages:reset']);
+  gulp.watch(['src/assets/scss/**/{*.scss, *.sass}'], ['sass']);
+  gulp.watch(['src/assets/js/**/*.js'], ['javascript']);
+  gulp.watch(['src/assets/img/**/*'], ['images']);
+  gulp.watch(['src/styleguide/**'], ['styleguide']);
+});
+
+// run an accessibility check with 'npm start a11y'
+gulp.task('a11y', function() {
+  gulp.run('pages');
+  return gulp.src('dist/*.html')
+  .pipe(access({accessibilityLevel: 'WCAG2A'}))
+//.pipe(arialinter({level: 'AA'}))
+  done();
 });
