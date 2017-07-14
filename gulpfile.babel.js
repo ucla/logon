7ab6bfa5a7c809299529e93e668b7838335a004b
Evan Jenkins <evan@activelamp.com>
2017-07-13 18:05:54 -0700

    fixing gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -104,7 +104,7 @@ function fa() {
 // Compile Sass into CSS
 // In production, the CSS is compressed
 function sass() {
-  return gulp.src('src/assets/scss/app.scss')
+  return gulp.src(['src/assets/scss/app.scss', 'src/assets/scss/errors.scss'])
     .pipe($.sourcemaps.init())
     .pipe($.sass({
       includePaths: PATHS.sass
